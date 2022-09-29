0ea423e796caa1c396651e47ddc19caa1b40bd7b
Stormy 606070 <stormy606070@gmail.com>
2022-09-29 19:04:42 +0100

    manual mode tos message updated

--- commands/guildmemberadd.js
+++ commands/guildmemberadd.js
@@ -7,7 +7,7 @@ module.exports = {
   description: 'Runs upon a guild member being added',
   execute(member, client){
     if (manual) {
-        member.send("Welcome to the Plot Block [LIFESTEAL] server!\nAt this time, you must be manually verified by an admin to play the server")
+        member.send("Welcome to the Plot Block [LIFESTEAL] server!\nAt this time, you must be manually verified by an admin to play the server.\nThe admin will send you a TOS and in order to be verified, you must read and accept it.")
     } else {
       const row = new ActionRowBuilder()
         .addComponents(
