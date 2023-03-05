572e6c228cb275d8af9f730d745790bcde706a0c
Stormy 606070 <stormy606070@gmail.com>
2023-03-05 19:03:44 +0000

    hope

---
+++ commands/clear.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('clear')
+		.setDescription('Clear an amount of messages')
+		.addNumberOption(option => 
+			option
+				.setName("amount")
+				.setDescription("The amount of messages to clear")
+			)
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+		.setDMPermission(false)
+		,
+	async execute(interaction, client) {
+		var amnt = parseInt(interaction.options.getNumber("amount"))
+		interaction.channel.bulkDelete(amnt)
+		interaction.reply({content:amnt+" message(s) have been cleared!"})
+	},
+};
