773462bfffb66a60aa14319e8c08ccc2a9c8268e
Stormy 606070 <stormy606070@gmail.com>
2023-03-08 18:11:51 +0000

    i have 13.4 intestinal diseases

--- commands/kick.js
+++ commands/kick.js
@@ -20,9 +20,8 @@ module.exports = {
 		.setDMPermission(false)//uwu
 		,
 	async execute(interaction, client) {
-		var user = parseInt(interaction.options.getUser("user"))
-		var reason = parseInt(interaction.options.getString("reason"))
-		console.log(user, " uwuwuwuwuwuwu ", reason)
+		var user = interaction.options.getUser("user")
+		var reason = interaction.options.getString("reason")
 		if (!reason) {
 			user.send({content: "***You have been kicked by `"+interaction.user.username+"` from Plot Block [LIFESTEAL]!***\nThere is no reason given for such an outlandish action.\nYou can rejoin using the link you have used previously, if it is still active."}
 			) .then(() => {
