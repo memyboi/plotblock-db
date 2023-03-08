52c56e4d48d3a477cd2cf4502b2804522ae6d034
Stormy 606070 <stormy606070@gmail.com>
2023-03-08 20:42:03 +0000

    uwuwuwwasdffsdfgdfgg

--- commands/warn.js
+++ commands/warn.js
@@ -21,12 +21,14 @@ module.exports = {
 		,
 	async execute(interaction, client) {
 		var user = interaction.options.getUser("user")
-		var reason = interaction.options.getUser("reason")
+		var reason = interaction.options.getString("reason")
 		
 		if (!reason) {
 			user.send({content: "*You have been warned by `"+interaction.user.username+"` from Plot Block [LIFESTEAL]!*\nThere is no reason given for such an outlandish action.\nPlease refrain from breaking the rules once more. If you feel you are not familiar with the rules, please re-read the rules."})
+			interaction.reply({content: "User has been warned. No reason given.", ephemeral: true})
 		} else {
 			user.send({content: "*You have been warned by `"+interaction.user.username+"` from Plot Block [LIFESTEAL]!*\nThe reason given is: \n```"+reason+"```\nPlease refrain from breaking the rules once more. If you feel you are not familiar with the rules, please re-read the rules."})
+			interaction.reply({content: "User has been warned. Reason has been given:\n```"+reason+"```", ephemeral: true})
 		}
 		// this part will add a record of this into the users data
 		// return to this when data is being worked on.
