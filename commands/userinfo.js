48d2cde3cae9509cf127429ba32fa16c99dc26f8
Stormy 606070 <stormy606070@gmail.com>
2023-03-09 21:01:25 +0000

    change

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -15,22 +15,30 @@ module.exports = {
 	async execute(interaction, client) {
 		var user = interaction.options.getUser("user")
 		client.guilds.fetch(""+process.env.guildid) .then((guild) => {
-			guild.members.fetch(""+user.id) .then((member) => {
-				let teamname = "null"
-				let cash = "null"
-				let xp = "null"
-				let lvls = "null"
-				let mcname = ""
-				const memberRoles = member.roles.cache
-				.filter((roles) => roles.id !== interaction.guild.id)
-				.map((role) => role.toString());
+			guild.members.fetch(""+user.id) .then(async (member) => {
+				const plrSchema = require("../schema.js")
+				const findRes = await plrSchema.find({ userID: user.id })
+				try {
+					let teamname = "null"
+					let cash = findRes[0].cash
+					let xp = findRes[0].xp
+					let lvls = findRes[0].lvls
+					let mcname = "null"
+					
+					const memberRoles = member.roles.cache
+						.filter((roles) => roles.id !== interaction.guild.id)
+						.map((role) => role.toString());
 
-				const exampleEmbed = new EmbedBuilder()
-				.setAuthor({ name: user.username, iconURL: member.displayAvatarURL()})
-				.setDescription(`${user.username}'s information:\n> minecraft name => ${mcname}\n> roles => ${memberRoles}\n> team => ${teamname}\n> cash => ${cash}\n> xp => ${xp}\n> lvls => ${lvls}`)
-				.setColor("#ff0000")
-				
-				interaction.reply({embeds: [exampleEmbed], ephemeral: true});
+					const exampleEmbed = new EmbedBuilder()
+						.setAuthor({ name: user.username, iconURL: member.displayAvatarURL()})
+						.setDescription(`${user.username}'s information:\n> minecraft name => ${mcname}\n> team => ${teamname}\n> cash => ${cash}\n> xp => ${xp}\n> lvls => ${lvls}\n> roles => ${memberRoles}`)
+						.setColor("#ff0000")
+					
+					interaction.reply({embeds: [exampleEmbed], ephemeral: true});
+				} catch(e) {
+					console.log(e)
+					interaction.reply({content: user.username+" does not have any user data!", ephemeral: true});
+				}
 			})
 		})
 
