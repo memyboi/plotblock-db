02e43403bcaa8f34744df186175fd53fd867cfc8
Stormy 606070 <stormy606070@gmail.com>
2023-03-12 22:06:33 +0000

    mute commands reinstalled

--- commands/mute.js
+++ commands/mute.js
@@ -42,9 +42,7 @@ module.exports = {
 		var reason = interaction.options.getString("reason")
 		var length = interaction.options.getString("time")
 
-		console.log("length: "+length)
-		console.log("length ms1: "+ms(length))
-		console.log("length ms2: "+ms(ms(length)))
+		if (!ms(ms(length)) && length) return interaction.reply({content:"You have inputted an invalid amount of time!"}) 
 		
 		client.guilds.fetch(""+process.env.guildid) .then((guild) => {
 			var role = guild.roles.cache.find(role => role.id == "1084533678849392731")
@@ -60,9 +58,11 @@ module.exports = {
 							interaction.reply({content: "User has been muted for "+ms(ms(length))+". Reason has been given:\n```"+reason+"```", ephemeral: true})
 							user.send({content: "*You have been muted by `"+interaction.user.username+"` for "+ms(ms(length))+" from Plot Block [LIFESTEAL]!*\nThe reason given is: \n```"+reason+"```\nPlease refrain from breaking the rules once more. If you feel you are not familiar with the rules, please re-read the rules."}) .then((msg) => {
 								setTimeout(() => {
-									member.roles.remove(role)
-									member.roles.add(talkingrole)
-									msg.reply({content: "*This mute has expired.*"})
+									if (member.roles.cache.some(role => role.id == "1084533678849392731") && !member.roles.cache.some(role => role.id = "1084582068316549252")) {
+										member.roles.remove(role)
+										member.roles.add(talkingrole)
+										msg.reply({content: "*This mute has expired.*"})
+									}
 								}, timeout);
 							})
 							
@@ -71,14 +71,16 @@ module.exports = {
 							interaction.reply({content: "User has been muted for "+ms(ms(length))+". No reason given.", ephemeral: true})
 							user.send({content: "*You have been muted by `"+interaction.user.username+"` for "+ms(ms(length))+" from Plot Block [LIFESTEAL]!*\nThere is no reason given for such an outlandish action.\nPlease refrain from breaking the rules once more. If you feel you are not familiar with the rules, please re-read the rules."}) .then((msg) => {
 								setTimeout(() => {
-									member.roles.remove(role)
-									member.roles.add(talkingrole)
-									msg.reply({content: "*This mute has expired.*"})
+									if (member.roles.cache.some(role => role.id == "1084533678849392731") && !member.roles.cache.some(role => role.id = "1084582068316549252")) {
+										member.roles.remove(role)
+										member.roles.add(talkingrole)
+										msg.reply({content: "*This mute has expired.*"})
+									}
 								}, timeout);
 							})
 						} 
 					} else {
-						interaction.reply({content: "User may already be muted!", ephemeral: true})
+						interaction.reply({content: "User may already be muted! Use", ephemeral: true})
 					}
 				} else {
 					if (!member.roles.cache.some(role => role.id == "1084533678849392731") && member.roles.cache.some(role => role.id == "1084582068316549252")) {
---
+++ commands/unmute.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
+const ms = require("ms");
+
+let warn = class {
+    constructor(
+        reason,
+		id,
+		timestamp
+        ) {
+
+        this.reason = reason
+		this.timestamp = timestamp
+		this.id = id
+      }
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('mute')
+		.setDescription('Mute a user for a reason.')
+		.addUserOption(option => 
+			option
+				.setName("user")
+				.setDescription("The user to mute.")
+				.setRequired(true)
+			)
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+		.setDMPermission(false)
+		,
+	async execute(interaction, client) {
+		var user = interaction.options.getUser("user")
+
+		client.guilds.fetch(""+process.env.guildid) .then((guild) => {
+			var role = guild.roles.cache.find(role => role.id == "1084533678849392731")
+			var talkingrole = guild.roles.cache.find(role => role.id == "1084582068316549252")
+			guild.members.fetch(""+user.id) .then((member) => {
+				if (member.roles.cache.some(role => role.id == "1084533678849392731") && !member.roles.cache.some(role => role.id == "1084582068316549252")) {
+					//muted, unmute
+					member.roles.remove(role)
+					member.roles.add(talkingrole)
+					interaction.reply({content: "User has been unmuted.", ephemeral: true})
+					user.send({content: "*You have been unmuted by `"+interaction.user.username+"` from Plot Block [LIFESTEAL]!*\nPlease do re-read the rules so you do not get punished again."})
+				}
+			})
+		})
+	},
+};
