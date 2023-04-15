f74b9e81ce8ba8b682550ee2ea7c2c037a85f348
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-04-15 13:21:45 +0100

    test 3

--- commands/livemap.js
+++ commands/livemap.js
@@ -11,11 +11,13 @@ module.exports = {
         client.guilds.fetch(""+process.env.guildid) .then((guild) => {
             guild.members.fetch(""+interaction.user.id) .then((member) => {
                 const isVerifiedRole = guild.roles.cache.find(role => role.id == "1022631935614406730")
-                if (!member.roles.cache.has(isVerifiedRole)) {
-                    return interaction.reply({content: "You must verify in order to run this command!", ephemeral: true})
+                if (member.roles.cache.has(isVerifiedRole)) {
+                    interaction.reply({content: "The livemap is available [here](http://plotblock.my.pebble.host:8127/#).", ephemeral: true})
+                } else {
+                    interaction.reply({content: "You must verify in order to run this command!", ephemeral: true})
                 }
             })
         })
-		interaction.reply({content: "The livemap is available [here](http://plotblock.my.pebble.host:8127/#).", ephemeral: true})
+		
 	},
 };
