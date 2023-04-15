7a36bc7d39c7561dc66aeb8865c7052c6e6653f2
deadfry42 <75206733+memyboi@users.noreply.github.com>
2023-04-15 14:06:25 +0100

    verification check removed from serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -7,14 +7,6 @@ module.exports = {
 		.setDescription('Gives you the current minecraft server info.')
 		,
 	async execute(interaction, client) {
-		client.guilds.fetch(""+process.env.guildid) .then((guild) => {
-            guild.members.fetch(""+interaction.user.id) .then((member) => {
-                if (member.roles.cache.some(role => role.id == "1022631935614406730")) {
-                    interaction.reply({content: "The minecraft server has been created, but currently the server is not available outside of verification. The live map is available by running </livemap:1096754378012119040>.", ephemeral: true})
-                } else {
-                    interaction.reply({content: "You must verify in order to run this command!", ephemeral: true})
-                }
-            })
-        })
+		interaction.reply({content: "The minecraft server has been created, but currently the server is not available outside of verification. The live map is available by running </livemap:1096754378012119040>.\nFor verification, you will need to join the server at __plotblock.my.pebble.host__ and follow the (summarised) instructions in <#1025101523933466634>.", ephemeral: true})
 	},
 };
